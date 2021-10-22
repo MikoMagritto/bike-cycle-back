4478bd72a934ee177f07bb15f0a74d516e44110a
MikoMagritto <77416124+MikoMagritto@users.noreply.github.com>
2021-10-22 16:45:21 +0200

    firstcommit

---
+++ back/models/Bike.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bikeShema = new Schema ({
+
+    brand : {
+        type : String,
+    },
+
+    size : {
+        type: String,
+        enum : ["XS","S","M","L"]
+    },
+
+    free : {
+
+    },
+
+    currentPosition : {}
+})
+
+/*{
+  id:’_b1’,
+  Marque: "Lorem ipsum...",
+  taille: ‘S’
+  disponibilité:true
+  currentPosition: { 
+lattitude:48.8544,
+  	longitude:2.3488
+}
+}*/ 
